4ca11887c8d86c20357c765ab0dc6caef6f4be4c
crazyminerparody <wsholanky@gmail.com>
2026-03-18 13:27:43 +0100

    Initial commit

--- Java /Gate-two/level-4/arraysTask.py
+++
@@ -1,7 +0,0 @@
-def numbers_in_an_array():
-    number = [10,11,12]
-    list_of_numbers = []
-        for number in list_of_numbers:
-            list_of_numbers.append[number]
-    return list_of_numbers
-numbers_in_an_array()
--- Java /Gate-two/level-4/method.py
+++
@@ -1,3 +0,0 @@
-def get_Addition_Of_Two_Numbers(firstNumber, secondNumber):
-    result = firstNumber + secondNumber
-    return result
