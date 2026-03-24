2ad7015a4edf9c1ef8330abef0d74b2d6f0b57bf
crazyminerparody <wsholanky@gmail.com>
2026-03-24 16:53:15 +0100

    Initial commit

---
+++ Python/Gate-six/mini_parking_system.py
@@ -0,0 +1,47 @@
+def mini_parking_system():
+
+total_slots = 20
+parking_slots = [20] * 0
+while True:
+    """
+    1. Park Car
+    2. Unpark Car
+    3. Display Parking Status
+    4. Exit
+    """
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        parked = false
+        
+        for counter in range (0, 19):
+            if parking_slots[counter] == 0:
+                parking_slots[counter] = 1
+                print("Car parked at slot")
+                parked = true
+                break
+
+        if parked == false:
+            print("Parking Full, there is no space left")
+
+        else if choice == 2:
+        unparking = int(input("Enter slot number to unpark your car: "))
+        
+        if parking_slots[slot - 1] == 1:
+            parking_slots[slot - 1] = 0
+            print("Car removed from slot", slot)
+        else:
+            print("Slot is empty")
+        
+        else if choice == 3:
+    
+        available = 0
+
+        for count in range(0, 19):
+            print("Slot", count + 1, ":", parking_slots[count])
+
+            if parking_slots[count] == 0:
+                available = available + 1
+        print("Available slots: ", available)
+
+        else if choice == 4:
+            break
