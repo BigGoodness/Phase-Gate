617380cc0bedcd917f50d3738950ecc83d37aa94
crazyminerparody <wsholanky@gmail.com>
2026-03-26 14:26:43 +0100

    Initial commit

--- Python/Gate-eight/Testingcontactapp.py
+++ Python/Gate-eight/Testingcontactapp.py
@@ -2,4 +2,22 @@ from unittest import TestCase
 import contact_app
 
 
-class Testing
+class testingContact(TestCase):
+    def test_for_adding_contacts(self):
+        firstName = "John"
+        lastName = "Doe"
+        phoneNumber = "08012345678"
+        actual = contact_app.add_Contact(firstName, lastName, phoneNumber)
+        expected = "Contact Added successfully!!!"
+
+        self.assertEqual(actual,expected)
+
+    def test_for_removing_contacts(self):
+        firstName = "John"
+        lastName = "Doe"
+        phoneNumber = "08012345678"
+        actual = contact_app.remove_Contact(phoneNumber)
+        expected = "Contact removed successfully!!"
+
+        self.assertEqual(actual,expected)
+    
--- Python/Gate-eight/contact_app.py
+++ Python/Gate-eight/contact_app.py
@@ -1,24 +1,34 @@
-contacts_list = []
+first_name_contacts_list = []
+last_name_contacts_list = []
+phone_number_contacts_list = []
 
 def add_Contact(first_name, last_name, phone_number):
-    first_name = input("Enter First Name: ")
-    last_name = input("Enter Last Name: ")
-    phone_number = int(input("Enter Phone: "))
 
-    contacts_list.append(first_name, last_name, phone_number)
+    first_name_contacts_list.append(first_name)
+    last_name_contacts_list.append(last_name)
+    phone_number_contacts_list.append(phone_number)
     return "Contact Added successfully!!!"
 
 
 def remove_Contact(phone_number):
-    for contact in contacts:
-        if phone_number[contact] == contacts_list:
+    for contact in phone_number_contacts_list:
+        if contact == phone_number_contacts_list:
             contacts_list.remove(phone_number)
     return "Contact removed successfully!!"
     
 
 
-def find_Contact_By_Phone_Number(phone_Number):
-    for contact in contacts:
-        if contact[phone_number] == contacts_list:
-            return contacts_list[first_name], contacts_list[last_name]
-    
+#def find_Contact_By_Phone_Number(phone_Number):
+#    for contact in contacts:
+#        if contact[phone_number] == contacts_list:
+#            return first_name_contacts_list[first_name], last_name_contacts_list[last_name]
+#            return contact[phone_number]
+#
+#
+#def find_contact_by_first_name(first_name_contacts_list):
+#    for contact in contacts:
+#        if contact[first_name_contacts_list] == first_name_contacts_list:
+#            return contact[first_name_contacts_list], contact[last_name_contacts_list], "-" contact[phone_number_contacts_list]
+#
+#
+#def editContact(phone_number_contacts_list):
